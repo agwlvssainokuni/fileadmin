7cbdc5c5fdd69a594069e3d22104a84d15b6b5e5
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2012-12-22 21:23:55 +0900

    ファイル管理ツール * 時間計算ユーティリティ

---
+++ file_admin/time_utils.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2012 Norio Agawa
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+require 'time'
+
+module FileAdmin
+
+  # 時間ユーティリティ
+  module TimeUtils
+
+    # 起点時刻から差分時間を変異させた時刻を算出する。
+    def calculate_time(time, diff)
+      tm = time.strftime("%Y/%m/%d %H:%M:%S")
+      out = %x{date -d "#{tm} #{diff}" +"%Y/%m/%d %H:%M:%S" 2>&1}
+      unless $? == 0
+        raise ArgumentError, out
+      end
+      return Time.parse(out)
+    end
+
+  end
+end
---
+++ spec/time_utils_spec.rb
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2012 Norio Agawa
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+require 'time'
+require File.join(File.dirname(__FILE__), '../file_admin/time_utils')
+
+
+describe FileAdmin::TimeUtils do
+  include FileAdmin::TimeUtils
+
+  describe "calculate_time" do
+    subject { calculate_time(@time, value) }
+    before(:all) { @time = Time.parse("2010/10/10") }
+
+    context "計算可能" do
+      let(:value) { "1 year ago 1 month ago 1 day ago" }
+      it { should ==  Time.parse("2009/09/09") }
+    end
+
+    context "計算不可(形式不正で例外)" do
+      let(:value) { "invalid" }
+      it {
+        expect { subject }.to raise_error(ArgumentError)
+      }
+    end
+  end
+
+end
