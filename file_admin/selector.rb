9bba919bffaab3fe9898f2a34b382fef7c6becf1
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2012-12-22 21:32:14 +0900

    ファイル管理ツール * 対象抽出機能

---
+++ file_admin/selector.rb
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2012 Norio Agawa
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+require File.join(File.dirname(__FILE__), 'logger')
+require File.join(File.dirname(__FILE__), 'validation')
+require File.join(File.dirname(__FILE__), 'command')
+
+module FileAdmin
+
+  # 対象抽出
+  module Selector
+
+    # 対象を抽出してリストとして取得する。
+    def collect_targets()
+      list = []
+      Array(@pattern).each {|dirpat|
+        l = select_by_dir_pattern(dirpat)
+        list += l.sort[0..-(@exclude.to_i + 1)]
+      }
+      return list
+    end
+
+    # 閾値を指定して対象を抽出してリストとして取得する。
+    def collect_targets_by_threshold(threshold)
+      list = []
+      Array(@pattern).each {|prefix|
+        pos = (prefix =~ /\/$/ ? 0 : File.basename(prefix).length)
+        l = select_by_dir_pattern("#{prefix}*#{@suffix}")
+        list += l.sort.select {|name|
+          threshold > File.basename(name, @suffix.to_s)[pos..-1]
+        }
+      }
+      return list
+    end
+
+    # DIRパターンを指定して対象を取得する。
+    private
+    def select_by_dir_pattern(dirpat)
+      if @regexp.nil? || @regexp.empty?
+        return Dir.glob(dirpat)
+      else
+        re = Regexp.new(@regexp)
+        if @cond.nil? || @cond.empty?
+          return Dir.glob(dirpat).select {|name| name =~ re }
+        else
+          return Dir.glob(dirpat).select {|name|
+            next false unless name =~ re
+            eval(@cond)
+          }
+        end
+      end
+    end
+
+  end
+end
---
+++ spec/selector_spec.rb
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2012 Norio Agawa
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+require File.join(File.dirname(__FILE__), '../file_admin/logger')
+require File.join(File.dirname(__FILE__), '../file_admin/selector')
+
+
+FileAdmin::Logger.console_enabled = false
+FileAdmin::Logger.syslog_enabled = false
+
+
+describe FileAdmin::Selector do
+  include FileAdmin::Selector
+
+  before(:all) do
+    @logger = FileAdmin::Logger.new()
+    @list = [
+             "testdir/dir1/file11.txt",
+             "testdir/dir1/file12.txt",
+             "testdir/dir2/file21.txt",
+             "testdir/dir2/file22.txt",
+             "testdir/dir3/file31.lst",
+             "testdir/dir3/file32.lst"
+            ]
+  end
+
+  before(:each) do
+    @list.each {|file|
+      %x{mkdir -p #{File.dirname(file)}}
+      %x{touch #{file}}
+    }
+  end
+
+  after(:each) do
+    %x{rm -rf testdir}
+  end
+
+  describe "collect_targets" do
+    subject { collect_targets() }
+
+    context "@patternを指定(文字列)して絞込み" do
+      before do
+        @pattern = "testdir/*/*"
+      end
+      it { should =~ @list }
+    end
+    context "@patternを指定(配列)して絞込み" do
+      before do
+        @pattern = [
+                    "testdir/dir1/*", "testdir/dir2/*", "testdir/dir3/*"
+                   ]
+      end
+      it { should =~ @list }
+    end
+    context "@patternと@regexpを指定して絞込み" do
+      before do
+        @pattern = "testdir/*/*"
+        @regexp = "file(\\d{2})\\.txt$"
+      end
+      it { should =~ @list[0..3] }
+    end
+    context "@patternと@regexpと@condを指定して絞込み" do
+      before do
+        @pattern = "testdir/*/*"
+        @regexp = "file(\\d{2})\\.txt$"
+        @cond = "$1.to_i > 20"
+      end
+      it { should =~ @list[2..3] }
+    end
+    context "@pattern(文字列)と@excludeを指定して絞込み" do
+      before do
+        @pattern = "testdir/*/*"
+        @exclude = "1"
+      end
+      it { should =~ @list[0..4] }
+    end
+    context "@pattern(配列)と@excludeを指定して絞込み" do
+      before do
+        @pattern = [
+                    "testdir/dir1/*", "testdir/dir2/*", "testdir/dir3/*"
+                   ]
+        @exclude = "1"
+      end
+      it { should =~ [@list[0], @list[2], @list[4]] }
+    end
+  end
+
+  describe "collect_targets_by_threshold" do
+    subject { collect_targets_by_threshold("30") }
+
+    context "@patternを指定(文字列)して絞込み" do
+      before do
+        @pattern = "testdir/*/file"
+        @suffix = ".txt"
+      end
+      it { should =~ @list[0..3] }
+    end
+    context "@patternを指定(配列)して絞込み" do
+      before do
+        @pattern = [
+                    "testdir/dir1/file", "testdir/dir2/file", "testdir/dir3/file"
+                   ]
+        @suffix = ".txt"
+      end
+      it { should =~ @list[0..3] }
+    end
+    context "@patternと@regexpを指定して絞込み" do
+      before do
+        @pattern = "testdir/*/file"
+        @suffix = ".txt"
+        @regexp = "file(\\d1)\\.txt$"
+      end
+      it { should =~ [@list[0], @list[2]] }
+    end
+    context "@patternと@regexpと@condを指定して絞込み" do
+      before do
+        @pattern = "testdir/*/file"
+        @suffix = ".txt"
+        @regexp = "file(\\d1)\\.txt$"
+        @cond = "$1.to_i > 20"
+      end
+      it { should =~ [@list[2]] }
+    end
+  end
+end
