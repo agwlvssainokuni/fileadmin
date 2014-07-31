054a04231751a83a23dd93b1e6e57052693c0013
agwlvssainokuni <agw.lvs.sainokuni@gmail.com>
2014-07-31 23:38:00 +0900

    ファイル管理ツール * リモート退避対応

--- sample/createfile.sh
+++ sample/createfile.sh
@@ -4,7 +4,8 @@ basedir=$(dirname ${BASH_SOURCE[0]})
 
 mkdir -p ${basedir}/0file
 mkdir -p ${basedir}/1arch
-mkdir -p ${basedir}/2back
+mkdir -p ${basedir}/2back/work
+mkdir -p ${basedir}/3back/work
 
 touch ${basedir}/0file/foreach_$(date +%Y%m%d%H%M%S).txt
 touch ${basedir}/0file/aggregate_1.txt
--- sample/run_fileadmin.sh
+++ sample/run_fileadmin.sh
@@ -32,15 +32,47 @@ cat <<__CONFIG__
   grace_period: "1 day ago"
   to_dir: ${basedir}/2back
 
-- !ruby/object:CLEANUP
-  label: 削除テスト
+- !ruby/object:BACKUP
+  label: リモート退避テスト
   basedir: ${basedir}/2back
   pattern:
     - foreach_
     - aggregate_
   suffix: .zip
   tsformat: "%Y%m%d"
-  grace_period: "2 days ago"
+  grace_period: "2 day ago"
+  to_dir: ${basedir}/2back/work/
+
+- !ruby/object:FETCH
+  label: リモート退避テスト
+  basedir: ${basedir}/3back/work/
+  host: localhost
+  rdir: ${basedir}/2back/work/
+  pattern:
+    - foreach_*.zip
+    - aggregate_*.zip
+  ext: done
+  to_dir: ${basedir}/3back/
+
+- !ruby/object:CLEANUP
+  label: リモート退避テスト
+  basedir: ${basedir}/2back/work
+  pattern:
+    - foreach_
+    - aggregate_
+  suffix: .zip.done
+  tsformat: "%Y%m%d"
+  grace_period: "3 days ago"
+
+- !ruby/object:CLEANUP
+  label: 削除テスト
+  basedir: ${basedir}/3back
+  pattern:
+    - foreach_
+    - aggregate_
+  suffix: .zip
+  tsformat: "%Y%m%d"
+  grace_period: "4 days ago"
 __CONFIG__
 
 )
